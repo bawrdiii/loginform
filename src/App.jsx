a1ca6f71b7f877ccef485a0f8bfa5c6b37d9e6d2
bawrdiii <bardiabarbod@live.com>
2022-07-27 09:01:46 +0200

    Create style component for login form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginForm from "./components/Loginform";
+
+const App = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Loginform.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+const LoginForm = () => {
+
+    const [user, setUser] = useState(false)
+
+
+
+    return (
+        <>
+        <div className="container">
+            <form method="post">
+                
+            </form>
+        </div>
+        </>
+    )
+}
+export default LoginForm
